0d09a7046baab437354728b31423e454889540a4
nouxcode <perrault.meaghan@gmail.com>
2022-09-26 15:10:23 -0700

    we can click on a square to mark it

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const handleGamePlay = (index) => {
+    let updateBoard = [...squares]
+    // updateBoard[clickSquare] = "x"
+    updateBoard[index] = "x"
+    setSquares(updateBoard)
 
+    console.log(index)
+  }
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <div className="game_container">{squares.map((square, index) => {
+      <div className="game_container">{squares.map((squares, index) => {
         return (
-          <Square />
+          <Square squares={squares} handleGamePlay={handleGamePlay} key={index} index={index} />
         )
       })}</div>
 
