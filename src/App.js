23bd57d7281ae93ad5b87ef223111704e8fffe67
aribrashear <aribrashear@gmail.com>
2022-09-27 09:49:41 -0700

    Added winning functionality.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,41 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(0);
 
+  const [firstPlayerMoves, setFirstPlayerMoves] = useState([]);
+  const [secondPlayerMoves, setSecondPlayerMoves] = useState([]);
+
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const handleWin = () => {
+    // We need to access each index of the wins array, which will be another array:
+    // [0, 1, 2]
+    // We want to compare the array playermoves to the index of wins
+
+    for (let i = 0; i < wins.length; i++) {
+      const [a, b, c] = wins[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        alert("You win!");
+      }
+      // console.log("[a, b, c]");
+
+      // If what playermoves contains is === to [a,b,c] in wins, then you win
+    }
+  };
+  console.log(handleWin());
+
   const handleGamePlay = (index) => {
     let updateBoard = [...squares];
 
@@ -16,7 +51,10 @@ const App = () => {
     ) {
       updateBoard[index] = "🎃";
       setSquares(updateBoard);
+      setFirstPlayerMoves(firstPlayerMoves.concat(index));
       setTurn(turn + 1);
+      handleWin();
+      console.log("P1:", firstPlayerMoves);
     } else if (
       turn % 2 !== 0 &&
       updateBoard[index] !== "🎃" &&
@@ -24,28 +62,10 @@ const App = () => {
     ) {
       updateBoard[index] = "🎄";
       setSquares(updateBoard);
+      setSecondPlayerMoves(secondPlayerMoves.concat(index));
       setTurn(turn + 1);
-    }
-    let wins = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < wins.length; i++) {
-      const [a, b, c] = wins[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return alert("You Win!");
-      }
+      handleWin();
+      console.log("P2:", secondPlayerMoves);
     }
 
     console.log(index);
